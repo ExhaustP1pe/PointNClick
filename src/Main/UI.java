4bf0a4ec5d0123ce45ebbddaaa7653a6ed7e9752
Goose <124514884+ExhaustP1pe@users.noreply.github.com>
2025-10-19 15:02:18 +0200

    set up background panel and bgLabel - added marging between the text and the bgPanel

--- src/Main/UI.java
+++ src/Main/UI.java
@@ -6,17 +6,22 @@ import java.awt.*;
 public class UI
 {
     GameManager gm;
+
     JFrame window;
     public JTextArea messageTxt;
+    public JPanel[] bgPanel = new JPanel[10];
+    public JLabel[] bgLabel = new JLabel[10];
 
     public UI(GameManager gm)
     {
         this.gm = gm;
         createMainField();
+        createBackground();
+
         window.setVisible(true);
     }
 
-    public void createMainField()
+    public void createMainField()//Parameters for GameWindow & TextArea
     {
         window = new JFrame();
         window.setSize(800,600);
@@ -25,7 +30,7 @@ public class UI
         window.setLayout(null);
 
         messageTxt = new JTextArea("The quick brown fox jumps over the lazy dog");
-        messageTxt.setBounds(50,400,700,150);
+        messageTxt.setBounds(50,415,700,150);
         messageTxt.setBackground(Color.black);
         messageTxt.setForeground(Color.white);
         messageTxt.setEditable(false);
@@ -33,5 +38,19 @@ public class UI
         messageTxt.setWrapStyleWord(true);
         messageTxt.setFont(new Font("Propaganda",Font.PLAIN,26));
         window.add(messageTxt);
+    }
+
+    public void createBackground()
+    {
+        bgPanel[1] = new JPanel();
+        bgPanel[1].setBounds(50,50,700,350);
+        bgPanel[1].setBackground(Color.pink);
+        bgPanel[1].setLayout(null);
+        window.add(bgPanel[1]);
+
+        bgLabel[1] = new JLabel();
+        bgLabel[1].setBounds(0,0,700,350);
+
+
     }
 }
