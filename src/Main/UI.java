5ac3e09d2959fc608bb1d59b931155958b6cccf0
Goose <124514884+ExhaustP1pe@users.noreply.github.com>
2025-10-19 20:51:59 +0200

    made createObject method [creates new objects for scene] - added assets for use

--- src/Main/UI.java
+++ src/Main/UI.java
@@ -16,21 +16,20 @@ public class UI
     {
         this.gm = gm;
         createMainField();
-        createBackground();
-
+        generateScreen();
         window.setVisible(true);
     }
 
     public void createMainField()//Parameters for GameWindow & TextArea
     {
         window = new JFrame();
-        window.setSize(800,600);
+        window.setSize(800,620);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.BLACK);
         window.setLayout(null);
 
         messageTxt = new JTextArea("The quick brown fox jumps over the lazy dog");
-        messageTxt.setBounds(50,415,700,150);
+        messageTxt.setBounds(50,425,700,150);
         messageTxt.setBackground(Color.black);
         messageTxt.setForeground(Color.white);
         messageTxt.setEditable(false);
@@ -40,17 +39,44 @@ public class UI
         window.add(messageTxt);
     }
 
-    public void createBackground()
+    public void createBackground(int bgNum,String bgFileName)
     {
-        bgPanel[1] = new JPanel();
-        bgPanel[1].setBounds(50,50,700,350);
-        bgPanel[1].setBackground(Color.pink);
-        bgPanel[1].setLayout(null);
-        window.add(bgPanel[1]);
+        bgPanel[bgNum] = new JPanel();
+        bgPanel[bgNum].setBounds(50,55,700,350);
+        bgPanel[bgNum].setBackground(Color.pink);
+        bgPanel[bgNum].setLayout(null);
+        window.add(bgPanel[bgNum]);
 
-        bgLabel[1] = new JLabel();
-        bgLabel[1].setBounds(0,0,700,350);
+        bgLabel[bgNum] = new JLabel();
+        bgLabel[bgNum].setBounds(0,0,700,350);
 
+        ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource(bgFileName));
+        bgLabel[bgNum].setIcon(bgIcon);
+
+
+    }
+
+    public void createObject(int bgNum,int objx,int objy,int objWidth,int objHeight,String objFileName)
+    {
+        JLabel objectLabel = new JLabel();
+        objectLabel.setBounds(objx,objy,objWidth,objHeight);
+
+        ImageIcon objectIcon = new ImageIcon(getClass().getClassLoader().getResource(objFileName));
+        objectLabel.setIcon(objectIcon);
+
+        bgPanel[bgNum].add(objectLabel);
+        bgPanel[bgNum].add(bgLabel[bgNum]);
+    }
+
+    public void generateScreen()
+    {
+       //SCREEN1
+        createBackground(1,"Backgrounds/forestbg.png");
+        createObject(1, 400,10,617,309,"Objects/icons/greenhouse.png");
+        createObject(1,100,170,144,144,"Character sprites/creature-sheet/00_creature-sheet.png");
+        createObject(1,250,220,100,100,"Objects/icons/chest1.png");
+        createObject(1,-185,-150,309,463,"Objects/icons/tree3.png");
+        createObject(1,60,225,232,77,"Objects/icons/fence.png");
 
     }
 }
